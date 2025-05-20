53da48c2bc3d35e70d5d8b4937f5e1f3d102ccf7
danirando <danielerando@tiscali.it>
2025-05-20 14:48:27 +0200

    completato main

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,35 @@
 export default function Main() {
     return (
     <main>
-        <h1>main</h1>
+        <div className="container-black">
+            <div className="container">
+                <h1>--&gt; Content goes here &lt;--</h1>
+            </div>  
+        </div>
+        <div className="container-blue">
+            <div className="container">
+                <div className="col-20">
+                    <img src="../src/assets/img/buy-comics-digital-comics.png" alt="" />
+                    <p>DIGITAL COMICS</p>
+                </div>
+                <div className="col-20">
+                    <img src="../src/assets/img/buy-comics-merchandise.png" alt="" />
+                    <p>DC MERCHANDISE</p>
+                </div>
+                <div className="col-20">
+                    <img src="../src/assets/img/buy-comics-subscriptions.png" alt="" />
+                    <p>SUBSCRIPTION</p>
+                </div>
+                <div className="col-20">
+                    <img src="../src/assets/img/buy-comics-shop-locator.png" alt="" />
+                    <p>COMIC SHOP LOCATOR</p>
+                </div>
+                <div className="col-20">
+                    <img src="../src/assets/img/buy-dc-power-visa.svg" alt="" />
+                    <p>DC POWER VISA</p>
+                </div>
+            </div>       
+        </div>
     </main>
     )
 }
