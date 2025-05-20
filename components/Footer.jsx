9286ee7aa604ea8f888a3b44969b9ef808083a5a
danirando <danielerando@tiscali.it>
2025-05-20 15:25:40 +0200

    completata prima parte footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,63 @@
 export default function Footer() {
-    return (<header><h1>ciaooo</h1></header>)
+    return (
+        <footer>
+            <div className="container top">
+                <div className="col-50 link">
+                    <div className="col-25">
+                        <ul>
+                            <li>
+                                <a href="#"><h1>DC COMICS</h1></a>
+                                <a href="#">Characters</a>
+                                <a href="#">Comics</a>
+                                <a href="#">Movies</a>
+                                <a href="#">TV</a>
+                                <a href="#">Games</a>
+                                <a href="#">Videos</a>
+                                <a href="#">News</a>
+                                <a href="#"><h1>SHOP</h1></a>
+                                <a href="#">Shop DC</a>
+                                <a href="#">Shop DC Collectibles</a>
+
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-25">
+                        <ul>
+                            <li>
+                                <a href="#"><h1>DC</h1></a>
+                                <a href="#">Terms Of Use</a>
+                                <a href="#">Privacy policy (New)</a>
+                                <a href="#">Ad Choices</a>
+                                <a href="#">Advertising</a>
+                                <a href="#">Subscription</a>
+                                <a href="#">Talent Workshops</a>
+                                <a href="#">CPSC Certificates</a>
+                                <a href="#">Ratings</a>
+                                <a href="#">Shop Help</a>
+                                <a href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-25">
+                        <ul>
+                            <li>
+                                <a href="#"><h1>SITES</h1></a>
+                                <a href="#">DC</a>
+                                <a href="#">MAD Magazine</a>
+                                <a href="#">DC Kids</a>
+                                <a href="#">DC Universe</a>
+                                <a href="#">DC Power Visa</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-25">
+
+                    </div>
+                </div>
+                <div className="col-50">
+                    <img className="logo" src="../src/assets/img/dc-logo-bg.png" alt="" />
+                </div>
+            </div>
+        </footer>
+    )
 }
