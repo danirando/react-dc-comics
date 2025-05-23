5a232ae37ddf679242177768b8ad737e1d5b9704
danirando <danielerando@tiscali.it>
2025-05-23 09:30:49 +0200

    add tag a in cards

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,8 +1,10 @@
 export default function Cards({ data }) {
   return data.map((data) => (
     <div className="card" key={data.id}>
-      <img src={data.thumb} alt={data.series} />
-      <h4>{data.series}</h4>
+      <a href="#">
+        <img src={data.thumb} alt={data.series} />
+        <h4>{data.series}</h4>
+      </a>
     </div>
   ));
 }
